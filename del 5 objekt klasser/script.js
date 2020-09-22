4bb10b4a7eae8823421c2e3a4cea8e90e6df6f3f
linneagunnarsson01 <69903237+linneagunnarsson01@users.noreply.github.com>
2020-09-22 15:31:58 +0200

    done

--- del 5 objekt klasser/script.js
+++ del 5 objekt klasser/script.js
@@ -3,29 +3,28 @@ var myObject = {
 
    title: "mat är gott" , //receptnamn
    rating: "fem av fem" , //betyg
+   servings: "4 pers", //portioner
     //lista ingridienser
-   ingredients: [
-    "2 1/2 dl vetemjöl",
-    "1/2 tsk salt",
-    "6 dl mjölk",
-    "3 ägg",
-    "3 msk smör (1 msk motsvarar ca 15 g)",
-    "sylt, bär eller frukt till servering"
-],
+    ingredients: {
+        ingredient:["vetemjöl", "salt", "mjölk", "ägg", "smör",  "sylt, bär eller frukt till servering" ], // array med ingridienserna
+        amount:["2 1/2 dl", "1/2 tsk", "6 dl","3", "3 msk", "" ] // array med mängd ingridienser
+   
+   },
  //lista steg för steg
    steps: [
     "Blanda mjöl och salt i en bunke. Vispa i hälften av mjölken och vispa till en slät smet. Vispa i resten av mjölken och äggen.",
     "Smält smöret i stekpannan och vispa ner i smeten. Stek tunna pannkakor av smeten i en stek- eller pannkakspanna.",
     "Servera med sylt, bär eller frukt."
-],
+    ],
 
- }
+}
  
- console.log(myObject.title) //skriver titel
- console.log(myObject.rating) // skriver betyg
+console.log(myObject.title) //skriver titel
+console.log(myObject.rating) // skriver betyg
 
-for (let i=0; i < myObject.ingredients.length; i++ ){
-    console.log(myObject.ingredients[i]) //skriver ut ingridiens
+for (let i=0; i < myObject.ingredients.ingredient.length; i++ ){
+    console.log(myObject.ingredients.amount[i] +" "+ myObject.ingredients.ingredient[i]) //skriver ut mängd sen ingridiens
+    
 
 }
 
