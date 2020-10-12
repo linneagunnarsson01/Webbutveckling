67c40767d5a9905eb144ff42ab34ceebae472fff
linneagunnarsson01 <69903237+linneagunnarsson01@users.noreply.github.com>
2020-10-12 10:38:29 +0200

    .

--- Uppgift 9/script.js
+++ Uppgift 9/script.js
@@ -13,12 +13,96 @@ var msgElem; //referens till div-elementyet för meddelanden.
 function init() {
 
 	let i; // loopvariabel
-	let startGameBtn; // referens till startknappen
+	let startGameBtn = document.getElementById("startGameBtn"); // referens till startknappen
+	startGameBtn.onclick = startGame;
 	let letterButtons; //array med referenser till bokstavsknapparna
-	
 	wordList = ["BLOMMA","LASTBIL","SOPTUNNA","KÖKSBORD","RADIOAPPARAT","VINTER","SOMMAR","DATORMUS","LEJON","ELEFANTÖRA","JULTOMTE",
 				"SKOGSHYDDA","BILNUMMER","BLYERTSPENNA","SUDDGUMMI","KLÄDSKÅP","VEDSPIS","LJUSSTAKE","SKRIVBORD","ELDGAFFEL","STEKPANNA",
 				"KASTRULL","KAFFEBRYGGARE","TALLRIK","SOFFBORD","TRASMATTA","FLYGPLAN","FLYGPLATS","TANGENTBORD"];
 	
+
+	letterButtons= document.getElementById("letterButtons").getElementsByTagName("button");
+	for(i=0; i<letterButtons.length; i++) letterButtons[i].onclick = guessLetter;
+
+// bild och element för meddelanden
+	hangmanImg = document.getElementById("hangman"); // referens till img element hangmanImg
+	msgElem = document.getElementById("message"); //referens till msgElem
+
+	
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
+
+// initiera ett nytt spel, välj ord, visa bokstavsrutor.
+// visa första bilden (tom bild) och sätt bildnummer till 0
+
+function startGame(){
+	hangmanImg.src = "pics/h0.png"; // visar bilden h0.png
+	hangmanImgNr = 0; // variablen hangmanimgnr tillelas värdet 0
+	randomWord(); // anropar funktionen randomworld
+	showLetterBoxes(); // anropar funktionen showletterboxes
+	
+
+
+} // avslutar endgame
+
+function randomWord(){
+	let wordIndex; 
+
+	wordIndex = Math.floor(Math.random()*wordList.length);
+	console.log(wordIndex);
+	selectedWord = wordList[wordIndex];
+
+
+}
+
+
+function showLetterBoxes(){
+	let newCode;
+	let i; // loopvariabel
+
+	newCode = ""; 
+	for ( i = 0; i < selectedWord.length; i++ ){
+		newCode += "<span>&nbsp;</span>";	
+	}
+
+	document.getElementById("letterBoxes").innerHTML = newCode;
+	letterBoxes = document.getElementById("letterBoxes").getElementsByTagName("span");
+
+}
+
+function guessLetter(){
+	let letter; // ska vara bokstaven för knappen 
+	let i; //loopvariabel 
+	let letterFound; // användas för flagga true/false
+	let correctLettersCount; // ska vara en räknare
+
+	letter = this.value;
+	letterFound = false;
+
+	for( i = 0; i< selectedWord.length; i++){
+		if(letter === selectedWord.charAt(i)){
+			letterBoxes[i].innerHTML = letter;
+			letterFound = true;
+		}
+
+	}
+
+	if(letterFound === false){
+		hangmanImgNr++; // ändrar bild som visas när man skrivit fel bokstav
+		hangmanImg.src = "pics/h" + hangmanImgNr + ".png";
+
+		if(hangmanImgNr === 6){
+			endgame(true);
+
+		}
+
+	}
+
+
+
+
+}
+
+function endgame(manHanged){
+	
+}
