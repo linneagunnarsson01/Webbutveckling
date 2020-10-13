c857f3f47329a90c6d09358b1c4514f8cf2dbc08
linneagunnarsson01 <69903237+linneagunnarsson01@users.noreply.github.com>
2020-10-13 15:58:33 +0200

    changes

--- balloon/js.js
+++ balloon/js.js
@@ -1,2 +1,36 @@
 
+let p = document.querySelector("p");
+let size;
+
+function Size(changeSize){
+    size = changeSize;
+    p.style.fontSize = size + "px";
+
+}
+
+Size(50);
+
+function Pil(event){
+    if (event.key === "ArrowUp"){
+
+        // Om size( motsvarar ballongens storlek) blir stärre än 400 px kommer den byta till explotionemoley
+        if(size> 400){
+            p.textContent = "💥";
+
+            document.body.removeEventListener("keydown", Pil);
+            // om inte size är över 400 px kommer ballongens storlek öka med 10% om användaren trycker arrowUp
+        }else {
+            Size(size *1.1); // Ökar ballongens storlek med 10% när användaren anänder ArrowUp
+            event.preventDefault();
+        }
+
+     // om inte size är över 400 px kommer ballongens storlek minska med 10% när användaren trycker ArrowDown
+    }else if(event.key === "ArrowDown"){
+        Size(size * 0.9); //Minskar ballongens storlek med 10% när användaren tar ArrowDown
+        event.preventDefault();
+    }
+
+}
+
+document.body.addEventListener("keydown", Pil);
 
